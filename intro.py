baa12275a3fc514ffb3e66e1d62606d0baa3222a
Zaynab Amimi <zaynabamimi27@gmail.com>
2025-12-05 04:29:07 +0100

    a

--- intro.py
+++ intro.py
@@ -1,1 +1,8 @@
-import pygam
+import pygame
+def show_intro_screen(screen,clock,bg_c,black):
+    intro=True
+    font_1= pygame.font.SysFont(None,60)
+    font_2= pygame.font.SysFont(None,36)
+    title=font_1.render("🍹 Falling Fruits 🍎", True, black)
+    subtitle=font_2.render()
+
